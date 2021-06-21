284901d0a8d4efcae12911e97982340502369791
Chester Garett A Calingacion <chestergarett@gmail.com>
2021-06-22 00:36:18 +0800

    pushing coding sessions

---
+++ calingacion-chester-garett/js-challenges-and-projects-v.1.2/react_training/my-app/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+import NewExpense from './components/NewExpense/NewExpense'
+import Expenses from'./components/Expenses/Expenses';
+
+const DUMMY_EXPENSES = [
+  {id: 'e1', title: 'Queen\'s Wings', amount: 100000, date: new Date(2021,1,28)},
+  {id: 'e2', title: 'Oracle', amount: 101.5, date: new Date(2021,2,15)},
+  {id: 'e3', title: 'Golden Staff', amount: 60.50, date: new Date(2021,3,10)},
+  {id: 'e4', title: 'Blades of Despair', amount: 120.4, date: new Date(2021,4,13)}
+  ]
+  
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+
+  const addExpenseHandler = (expense) =>{
+    setExpenses((prevExpenses) =>{
+      return [expense, ...prevExpenses]
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense = {addExpenseHandler}/>
+      <Expenses items={expenses}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ calingacion-chester-garett/js-challenges-and-projects-v.1.2/react_training/my-app/src/components/Expenses/Expenses.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import ExpensesList from './ExpensesList';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpenseFilter';
+import './Expenses.css'
+import ExpensesChart from './ExpensesChart'
+
+const Expenses = (props) =>{
+    const [filteredYear,setFilteredYear] = useState('2021')
+    
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
+
+    const filteredExpenses = props.items.filter(expense=>{
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+       return (
+        <li>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses}/>
+            </Card>
+        </li>)
+}
+
+export default Expenses;
